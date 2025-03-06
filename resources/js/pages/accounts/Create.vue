fbe2f31d93f98c3802be925eb323e638a3a7809e
Ben Yarrow <ben.yarrow@enovate.co.uk>
2025-03-06 21:23:28 +0000

    Add create form

---
+++ resources/js/pages/accounts/Create.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head, usePage } from '@inertiajs/vue3';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
+
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Create Account',
+        href: '/accounts',
+    },
+];
+
+const page = usePage();
+console.log(page.props['csrf_token'])
+</script>
+
+<template>
+    <Head title="Create Account" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="p-8 lg:p-12">
+            <Form
+                method="post"
+                :action="route('accounts.store')"
+            >
+                <input type="hidden" name="_token" :value="page.props['csrf_token']" />
+
+                <Label for="name">Name</Label>
+                <Input
+                    name="name"
+                    type="text"
+                />
+
+                <Label for="company">Company</Label>
+                <Input
+                    name="company"
+                    type="text"
+                />
+
+                <Label for="type">Type</Label>
+                <Input
+                    name="type"
+                    type="text"
+                />
+
+                <Button type="submit">
+                    Create
+                </Button>
+            </Form>
+        </div>
+    </AppLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,21 @@ Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+    Route::name('accounts.')->prefix('/accounts')->group(function () {
+        Route::get('/', [AccountsController::class, 'index'])->name('index');
+
+        Route::get('/create', [AccountsController::class, 'create'])->name('create');
+        Route::post('/', [AccountsController::Class, 'store'])->name('store');
+
+        // Route::get('/{account}/edit', [AccountsController::class, 'edit'])->name('edit');
+        // Route::get('/{account}/edit', [AccountsController::class, 'edit'])->name('edit');
+    });
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
